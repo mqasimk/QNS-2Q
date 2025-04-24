0aabe09247614bca63315055ef9d1304b263833a
mqasimk <mqasimk.96@gmail.com>
2025-04-24 12:32:18 -0400

    Resolving outstanding issues.

---
+++ utils/spectra.py
@@ -0,0 +1,101 @@
+import jax.numpy as jnp
+import jax
+
+
+
+@jax.jit
+def L(w, w0, tc):
+    """
+    Generate a symmetric Lorentzian function.
+    :param w: ndarray of the frequencies at which to evaluate the Lorentzian.
+    :param w0: float with the location of the peak in frequency space.
+    :param tc: float with the correlation time for the noise process, which determines the width of the Lorentzian.
+    :return: ndarray of the value of the function.
+    """
+    return 0.5*(1/(1+(tc**2)*(w-w0)**2)+1/(1+(tc**2)*(w+w0)**2))
+
+
+@jax.jit
+def Gauss(w, w0, sig):
+    """
+    A Gaussian function that is not normalized.
+    :param w: ndarray of the frequencies at which to evaluate the Gaussian.
+    :param w0: float with the peak of the Gaussian.
+    :param sig: float with the standard deviation of the Gaussian.
+    :return: ndarray of the value of the function.
+    """
+    return jnp.exp(-(w-w0)**2/(2*sig**2))
+
+
+@jax.jit
+def S_11(w):
+    """
+    Self spectrum for qubit 1
+    :param w: ndarray of the frequencies at which to evaluate the spectrum.
+    :return: ndarray of the value of the function.
+    """
+    tc=1.2e-6
+    S0 =1e3
+    St2 = 1e6
+    w0=0.75*10**7
+    return (S0*(2*L(w, 0, tc)+L(w, w0, tc)+Gauss(w, 1.5*w0, 1/tc)+1.3*L(w, 2.5*w0, 0.75*tc))
+            +St2*L(w, 0, 1e3*tc))
+
+
+@jax.jit
+def S_22(w):
+    """
+    Self spectrum for qubit 2
+    :param w: ndarray of the frequencies at which to evaluate the spectrum.
+    :return: ndarray of the value of the function.
+    """
+    tc=1.2e-6
+    S0 = 1.25e3
+    St2 = 1e6
+    w0=0.75*10**7
+    return (S0*(2*L(w, 0, tc)+L(w, 1.2*w0, tc)+(2/3)*Gauss(w, 2.5*w0, 2/tc))
+            +St2*L(w, 0, 1e3*tc))
+
+
+@jax.jit
+def S_1212(w):
+    """
+    Self spectrum for the ZZ (Ising) interaction
+    :param w: ndarray of the frequencies at which to evaluate the spectrum.
+    :return: ndarray of the value of the function.
+    """
+    tc=1.2e-6
+    S0 = 0.6e3
+    w0=1*10**7
+    return S0*L(w, w0, tc)+2*S0/(1+(2*tc*w))
+
+
+@jax.jit
+def S_1_2(w, gamma):
+    """
+    Cross spectrum for qubits 1,2
+    :param w: ndarray of the frequencies at which to evaluate the spectrum.
+    :return: complex valued ndarray of the value of the function.
+    """
+    return jnp.sqrt(S_11(w)*S_22(w))*jnp.exp(-1j*w*gamma)
+
+
+@jax.jit
+def S_1_12(w, gamma12):
+    """
+    Cross spectrum between qubit 1 and the Ising interaction
+    :param w: ndarray of the frequencies at which to evaluate the spectrum.
+    :return: complex valued ndarray of the value of the function.
+    """
+    return jnp.sqrt(S_11(w)*S_1212(w))*jnp.exp(-1j*w*gamma12)
+
+
+@jax.jit
+def S_2_12(w, gamma12):
+    """
+    Cross spectrum between qubit 2 and the Ising interaction
+    :param w: ndarray of the frequencies at which to evaluate the spectrum.
+    :return: complex valued ndarray of the value of the function.
+    """
+    return jnp.sqrt(S_22(w)*S_1212(w))*jnp.exp(-1j*w*gamma12)
+
--- utils/trajectories.py
+++ utils/trajectories.py
@@ -1,11 +1,18 @@
 import numpy as np
 import qutip as qt
-from scipy.linalg import expm
-from joblib import Parallel, delayed
 import jax
 import jax.numpy as jnp
+from spectra import S_11, S_22, S_1212
+
 
 def make_noise_mat_arr(act, **kwargs):
+    """
+    Generate the matrices that will be used to generate temporally-correlated noise trajectories to determine the
+    evolution of the qubit.
+    :param act: 'load', 'make' and 'save' can be input as actions.
+    :param kwargs: a dictionary of input parameters.
+    :return: ndarray of the noise matrices.
+    """
     spec_vec = kwargs.get('spec_vec')
     t_vec = kwargs.get('t_vec')
     w_grain = kwargs.get('w_grain')
@@ -16,12 +23,11 @@ def make_noise_mat_arr(act, **kwargs):
     if act == 'load':
         return np.load('noise_mats.npy', allow_pickle=True)
     elif act == 'make':
-        S, C = make_noise_mat(spec_vec[0], t_vec, w_grain = w_grain, wmax = wmax, trunc_n = truncate, gamma = 0)
-        Sg, Cg = make_noise_mat(spec_vec[0], t_vec, w_grain = w_grain, wmax = wmax, trunc_n = truncate, gamma = gamma)
-        S_12, C_12 = make_noise_mat(spec_vec[1], t_vec, w_grain = w_grain, wmax = wmax, trunc_n = truncate, gamma = 0)
-        Sg_12, Cg_12 = make_noise_mat(spec_vec[1], t_vec, w_grain = w_grain, wmax = wmax, trunc_n = truncate,
+        S_11, C_11 = make_noise_mat(spec_vec[0], t_vec, w_grain = w_grain, wmax = wmax, trunc_n = truncate, gamma = 0.)
+        S_22g, C_22g = make_noise_mat(spec_vec[1], t_vec, w_grain = w_grain, wmax = wmax, trunc_n = truncate, gamma = gamma)
+        S_1212g, C_1212g = make_noise_mat(spec_vec[2], t_vec, w_grain = w_grain, wmax = wmax, trunc_n = truncate,
                                       gamma = gamma_12)
-        return np.array([[S, C], [Sg, Cg], [S_12, C_12], [Sg_12, Cg_12]])
+        return jnp.array([[S_11, C_11], [S_22g, C_22g], [S_1212g, C_1212g]])
     elif act == 'save':
         mats = make_noise_mat_arr('make', **kwargs)
         np.save('noise_mats.npy', mats)
@@ -30,36 +36,82 @@ def make_noise_mat_arr(act, **kwargs):
         raise Exception("Invalid action input")
 
 
-def make_noise_mat(spec_vec, t_vec, **kwargs):
+# @jax.jit
+def sinM(spec, w, t, dw, gamma):
+    """
+    Utility function to generate the noise matrices
+    :param spec: function for the noise spectrum of the particular noise trajectory.
+    :param w: float at which to evaluate this function.
+    :param t: float at which to evaluate this function.
+    :param dw: float with the small increment in frequency space.
+    :param gamma: float with the translation in time.
+    :return: float
+    """
+    return jnp.sqrt(dw*spec(w)/jnp.pi)*jnp.sin(w*(t + gamma))
+
+
+# @jax.jit
+def cosM(spec, w, t, dw, gamma):
+    """
+    Utility function to generate the noise matrices
+    :param spec: function for the noise spectrum of the particular noise trajectory.
+    :param w: float at which to evaluate this function.
+    :param t: float at which to evaluate this function.
+    :param dw: float with the small increment in frequency space.
+    :param gamma: float with the translation in time.
+    :return: float
+    """
+    return jnp.sqrt(dw*spec(w)/jnp.pi)*jnp.cos(w*(t + gamma))
+
+
+def make_noise_mat(spec, t_vec, **kwargs):
+    """
+
+    :param spec: list of the qubit and Ising self-spectra.
+    :param t_vec: ndarray of times for the system evolution.
+    :param kwargs: 'w_grain' is the size of the matrix columns, 'gamma' is the shift in time, 'wmax' is the
+                    cutoff freq.
+    :return: sin and cosine matrices for the parameters provided.
+    """
     w_grain = kwargs.get('w_grain')
     wmax = kwargs.get('wmax')
-    # trunc_n = kwargs.get('trunc_n')
+    #trunc_n = kwargs.get('trunc_n')
     gamma = kwargs.get('gamma')
-    size_t = np.size(t_vec)
+    #size_t = np.size(t_vec)
     size_w = int(2*w_grain)
-    S = np.zeros((size_t, size_w))
-    C = np.zeros((size_t, size_w))
+    w = jnp.linspace(0, 2*wmax, size_w)
     dw = wmax/w_grain
-    for i in range(size_t):
-        for j in range(size_w):
-            S[i,j] = np.sqrt(dw*spec_vec(j*dw)/np.pi)*np.sin(j*dw*(t_vec[i] + gamma))
-            C[i,j] = np.sqrt(dw*spec_vec(j*dw)/np.pi)*np.cos(j*dw*(t_vec[i] + gamma))
-    return S, C
+    Sf = jax.vmap(jax.vmap(sinM, in_axes=(None, 0, None, None, None)), in_axes=(None, None, 0, None, None))
+    Cf = jax.vmap(jax.vmap(cosM, in_axes=(None, 0, None, None, None)), in_axes=(None, None, 0, None, None))
+    return Sf(spec, w, t_vec, dw, gamma), Cf(spec, w, t_vec, dw, gamma)
 
 
-def make_noise_traj(S_list, C_list):
-    S = S_list[0]
-    C = C_list[0]
-    Sg = S_list[1]
-    Cg = C_list[1]
-    A = np.array(np.random.normal(0, 1, (np.size(S,1), 1)))
-    B = np.array(np.random.normal(0, 1, (np.size(C,1), 1)))
-    traj = np.ndarray.flatten(np.matmul(S, A) + np.matmul(C, B))
-    traj_g = np.ndarray.flatten(np.matmul(Sg, A) + np.matmul(Cg, B))
-    return np.array(traj), np.array(traj_g)
+@jax.jit
+def make_noise_traj(S, C, key):
+    """
+    Use the generated noise matrices S and C to efficiently generate noise trajectories.
+    :param S: sine matrix as generated by the make_noise_mat method.
+    :param C: cosine matrix as generated by the make_noise_mat method.
+    :param key: the key is used to make sure that the randomly generated trajectories are generated from the same
+                distribution, ensuring correlations.
+    :return:
+    """
+    key1 = jax.random.PRNGKey(key[0])
+    A = jax.random.normal(key1, (jnp.size(S, 1), 1))
+    key2 = jax.random.PRNGKey(key[1])
+    B = jax.random.normal(key2, (jnp.size(S, 1), 1))
+    traj = jnp.ravel(jnp.matmul(S, A) + jnp.matmul(C, B))
+    return traj
 
 
 def make_init_state(a_sp, c, **kwargs):
+    """
+    Given a set of state-preparation errors, generate the initial state.
+    :param a_sp: state-prep params along z.
+    :param c: state-prep params along x and y.
+    :param kwargs: the 'state' to generated can be chosen from the set {'p0', 'p1', '0p', '1p', 'pp'}
+    :return: the two qubit state as a qutip Qobj
+    """
     zp = qt.basis(2, 0)
     zm = qt.basis(2, 1)
     x_gates = [qt.tensor(qt.sigmax(), qt.identity(2)), qt.tensor(qt.identity(2), qt.sigmax())]
@@ -67,9 +119,8 @@ def make_init_state(a_sp, c, **kwargs):
     asp_1 = a_sp[1]
     c_0 = c[0]
     c_1 = c[1]
-    basis2q = [qt.tensor(zp, zp), qt.tensor(zp, zm), qt.tensor(zm, zp), qt.tensor(zm, zm)]
-    rho0_0 = 0.5*(1.+asp_0)*zp*zp.dag() + 0.5*(1.-asp_0)*zm*zm.dag() + 0.5*c_0*zp*zm.dag() + 0.5*c_0.conj()*zm*zp.dag()
-    rho0_1 = 0.5*(1.+asp_1)*zp*zp.dag() + 0.5*(1.-asp_1)*zm*zm.dag() + 0.5*c_1*zp*zm.dag() + 0.5*c_1.conj()*zm*zp.dag()
+    rho0_0 = 0.5*(1.+asp_0)*zp*zp.dag() + 0.5*(1.-asp_0)*zm*zm.dag() + 0.5*c_0*zp*zm.dag() + 0.5*np.conj(c_0)*zm*zp.dag()
+    rho0_1 = 0.5*(1.+asp_1)*zp*zp.dag() + 0.5*(1.-asp_1)*zm*zm.dag() + 0.5*c_1*zp*zm.dag() + 0.5*np.conj(c_1)*zm*zp.dag()
     rho0 = qt.tensor(rho0_0, rho0_1)
     ry = [qt.tensor(np.cos(np.pi/4)*qt.identity(2) - 1j*np.sin(np.pi/4)*qt.sigmay(), qt.identity(2)),
           qt.tensor(qt.identity(2), np.cos(np.pi/4)*qt.identity(2) - 1j*np.sin(np.pi/4)*qt.sigmay())]
@@ -87,29 +138,19 @@ def make_init_state(a_sp, c, **kwargs):
         raise Exception("Invalid state input")
 
 
+@jax.jit
 def make_Hamiltonian(y_uv, b_t):
-    b_t_1 = b_t[0]
-    b_t_1_g = b_t[1]
-    b_t_2 = b_t[0]
-    b_t_2_g = b_t[1]
-    b_t_12 = b_t[2]
-    b_t_12_g = b_t[3]
-    z_vec = [qt.tensor(qt.identity(2), qt.identity(2)),
-             qt.tensor(qt.sigmaz(), qt.identity(2)),
-             qt.tensor(qt.identity(2), qt.sigmaz()),
-             qt.tensor(qt.sigmaz(), qt.sigmaz())]
-    h_t = []
-    B = [[qt.sigmax(), qt.sigmay()], [qt.sigmax(), qt.sigmay()], [qt.sigmax(), qt.sigmay()]]
-    b_vec = [[b_t_1, b_t_1_g], [b_t_2, b_t_2_g], [b_t_12, b_t_12_g]]
-    for i in range(3):
-        for j in range(3):
-            h_t.append([qt.tensor(z_vec[i], B[j][0]), y_uv[i,j]*b_vec[j][0]])
-            h_t.append([qt.tensor(z_vec[i], B[j][1]), y_uv[i,j]*b_vec[j][1]])
+    paulis = jnp.array([[[1., 0.], [0., 1.]], [[0., 1.], [1., 0.]], [[0., -1j], [1j, 0.]], [[1., 0.], [0., -1.]]])
+    z_vec = jnp.array([jnp.kron(paulis[0], paulis[0]), jnp.kron(paulis[3], paulis[0]), jnp.kron(paulis[0], paulis[3]),
+                       jnp.kron(paulis[3], paulis[3])])
+    h_t = (jnp.tensordot(y_uv[0, 0]*b_t[0]*0.5, jnp.kron(z_vec[1], paulis[0]), 0)
+           + jnp.tensordot(y_uv[1, 1]*b_t[1]*0.5, jnp.kron(z_vec[2], paulis[0]), 0)
+           + jnp.tensordot(y_uv[2, 2]*b_t[2]*0.5, jnp.kron(z_vec[3], paulis[0]), 0))
     return h_t
 
 
 def f(t, tk):
-    return sum([((-1)**i)*np.heaviside(t-tk[i],1)*np.heaviside(tk[i+1]-t,1) for i in range(np.size(tk)-1)])
+    return jnp.sum(jnp.array([((-1)**i)*jnp.heaviside(t-tk[i], 1)*jnp.heaviside(tk[i+1] - t, 1) for i in range(jnp.size(tk) - 1)]), axis=0)
 
 
 def cpmg(t, n):
@@ -134,7 +175,7 @@ def prim_cycle(ct):
     tk1 = [(k+0.5)*t[-1]/(4*m) for k in range(int(2))]
     tk1.insert(0,0.)
     tk1 = np.array(tk1)
-    tk2 = tk1 + t[-1]/2
+    tk2 = tk1 + t[-1]*0.5
     tk2 = np.concatenate((tk2, [t[-1]]))
     tk = np.concatenate((tk1, tk2))
     return f(t,tk)
@@ -152,102 +193,99 @@ def cdd3(t, m):
 def make_y(t_b, pulse, **kwargs):
     ctime = kwargs.get('ctime')
     M = kwargs.get('M')
-    n = int(t_b[-1]/ctime)
+    n = int((t_b[-1]/ctime).round(0))
     y = np.zeros((3,3,np.size(t_b)))
     for i in range(2):
         if pulse[i] == 'CPMG':
             y[i, i] = cpmg(t_b, n)
-            #y = y.at[i, i].set(cpmg(t_b, n))
         elif pulse[i] == 'CDD1':
             y[i, i] = cdd1(t_b, n)
-            #y = y.at[i, i].set(cdd1(t_b, n))
+        elif pulse[i] == 'CDD1-1/2':
+            n1 = int((t_b[-1]/(0.5*ctime)).round(0))
+            y[i, i] = cdd1(t_b, n1)
+        elif pulse[i] == 'CDD1-1/4':
+            n2 = int((t_b[-1]/(0.25*ctime)).round(0))
+            y[i, i] = cdd1(t_b, n2)
+        elif pulse[i] == 'CPMG-1/2':
+            n2 = int((t_b[-1]/(0.5*ctime)).round(0))
+            y[i, i] = cpmg(t_b, n2)
         elif pulse[i] == 'CDD3':
             y[i, i] = cdd3(t_b, n)
-            #y = y.at[i, i].set(cdd3(t_b, n))
         elif pulse[i] == 'FID':
             y[i, i] = np.ones(np.size(t_b))
-            #y = y.at[i, i].set(np.ones(np.size(t_b)))
         else:
-            raise Exception("Invalid pulse input")
-    y[2,2] = np.multiply(y[1,1], y[0,0])
-    #y = y.at[2, 2].set(np.multiply(y[1,1], y[0,0]))
+            raise ValueError("The input pulse sequence not recognized.")
+    y[2,2] = y[1, 1]*y[0, 0]
     return np.tile(y, M)
 
 
-def solver(y_uv, noise_mats, t_vec, **kwargs):
-    n_shots = kwargs.get('n_shots')
-    state = kwargs.get('state')
-    a_sp = kwargs.get('a_sp')
-    c = kwargs.get('c')
-    S, C = noise_mats[0,0], noise_mats[0,1]
-    Sg, Cg = noise_mats[1,0], noise_mats[1,1]
-    S_12, C_12 = noise_mats[2,0], noise_mats[2,1]
-    Sg_12, Cg_12 = noise_mats[3,0], noise_mats[3,1]
-    rho0 = make_init_state(a_sp, c, state = state)
-    rho_B = qt.basis(2, 0) * qt.basis(2, 0).dag()
-    output = []
-    for i in range(n_shots):
-        b_t, b_t_g = make_noise_traj([S, Sg], [C, Cg])
-        b_t_12, b_t_g_12 = make_noise_traj([S_12, Sg_12], [C_12, Cg_12])
-        rho = qt.tensor(rho0, rho_B)
-        H_t = make_Hamiltonian(y_uv, [b_t, b_t_g, b_t_12, b_t_g_12])
-        sol = qt.mesolve(H_t, rho, t_vec)
-        output.append(sol)
-    rho_MT = []
-    for i in range(n_shots):
-        rho_MT.append(output[i].states[-1])
-    return rho_MT
+def custom_y(vt, t_b, M):
+    y = jnp.zeros((3, 3, np.size(t_b)))
+    ftn = f(t_b, vt[0])
+    y = y.at[0, 0].set(ftn)
+    ftn = f(t_b, vt[1])
+    y = y.at[1, 1].set(ftn)
+    y = y.at[2, 2].set(jnp.multiply(y[1, 1], y[0, 0]))
+    return jnp.tile(y, M)
 
 
+@jax.jit
 def make_propagator(H_t, t_vec):
-    integrand = np.array([(-1j)*(np.array([H_t[i][1][j] * (H_t[i][0]).full() for j in range(H_t[i][1].shape[0])])) for i in range(len(H_t))])
-    integrand = np.sum(integrand, axis=0)
-    U = expm(np.trapz(integrand, t_vec, axis=0))
+    U = jax.scipy.linalg.expm(-1j*jax.scipy.integrate.trapezoid(H_t, t_vec, axis=0))
     return U
 
 
-def single_shot_prop(noise_mats, t_vec, y_uv, rho0):
-    # S, C = noise_mats[0,0], noise_mats[0,1]
-    # Sg, Cg = noise_mats[1,0], noise_mats[1,1]
-    # S_12, C_12 = noise_mats[2,0], noise_mats[2,1]
-    # Sg_12, Cg_12 = noise_mats[3,0], noise_mats[3,1]
-    b_t, b_t_g = make_noise_traj([noise_mats[0,0], noise_mats[1,0]], [noise_mats[0,1], noise_mats[1,1]])
-    b_t_12, b_t_g_12 = make_noise_traj([noise_mats[2,0], noise_mats[3,0]], [noise_mats[2,1], noise_mats[3,1]])
-    H_t = make_Hamiltonian(y_uv, [b_t, b_t_g, b_t_12, b_t_g_12])
+@jax.jit
+def b1(t, w, gamma, key):
+    key1 = jax.random.PRNGKey(key[0])
+    A = jax.random.normal(key1, jnp.shape(w))
+    key2 = jax.random.PRNGKey(key[1])
+    B = jax.random.normal(key2, jnp.shape(w))
+    return jnp.sum(jnp.sqrt((w[1]-w[0])*S_11(w)/jnp.pi)*(A*jnp.cos(w*(t + gamma))+B*jnp.sin(w*(t + gamma))), axis=0)
+
+
+@jax.jit
+def b2(t, w, gamma, key):
+    key1 = jax.random.PRNGKey(key[0])
+    A = jax.random.normal(key1, jnp.shape(w))
+    key2 = jax.random.PRNGKey(key[1])
+    B = jax.random.normal(key2, jnp.shape(w))
+    return jnp.sum(jnp.sqrt((w[1]-w[0])*S_22(w)/jnp.pi)*(A*jnp.cos(w*(t + gamma))+B*jnp.sin(w*(t + gamma))), axis=0)
+
+
+@jax.jit
+def b12(t, w, gamma, key):
+    key1 = jax.random.PRNGKey(key[0])
+    A = jax.random.normal(key1, jnp.shape(w))
+    key2 = jax.random.PRNGKey(key[1])
+    B = jax.random.normal(key2, jnp.shape(w))
+    return jnp.sum(jnp.sqrt((w[1]-w[0])*S_1212(w)/jnp.pi)*(A*jnp.cos(w*(t + gamma))+B*jnp.sin(w*(t + gamma))), axis=0)
+
+
+@jax.jit
+def single_shot_prop(noise_mats, t_vec, y_uv, rho0, key):
+    size = jnp.size(t_vec)
+    y_uv = y_uv[:, :, :size]
+    bvec_1 = make_noise_traj(noise_mats[0, 0], noise_mats[0, 1], key)[:size]
+    bvec_2_g = make_noise_traj(noise_mats[1, 0], noise_mats[1, 1], key)[:size]
+    bvec_1212g = make_noise_traj(noise_mats[2, 0], noise_mats[2, 1], key)[:size]
+    H_t = make_Hamiltonian(y_uv, jnp.array([bvec_1, bvec_2_g, bvec_1212g]))
     U = make_propagator(H_t, t_vec)
-    rho_MT = U @ rho0 @ U.conjugate().transpose()
-    return qt.Qobj(rho_MT, dims = [[2,2,2],[2,2,2]])
-
-# def solver_prop(y_uv, noise_mats, t_vec, **kwargs):
-#     n_shots = kwargs.get('n_shots')
-#     state = kwargs.get('state')
-#     a_sp = kwargs.get('a_sp')
-#     c = kwargs.get('c')
-#     S, C = noise_mats[0,0], noise_mats[0,1]
-#     Sg, Cg = noise_mats[1,0], noise_mats[1,1]
-#     S_12, C_12 = noise_mats[2,0], noise_mats[2,1]
-#     Sg_12, Cg_12 = noise_mats[3,0], noise_mats[3,1]
-#     rho0 = make_init_state(a_sp, c, state = state)
-#     rho_B = qt.basis(2, 0) * qt.basis(2, 0).dag()
-#     output = []
-#     for i in range(n_shots):
-#         b_t, b_t_g = make_noise_traj([S, Sg], [C, Cg])
-#         b_t_12, b_t_g_12 = make_noise_traj([S_12, Sg_12], [C_12, Cg_12])
-#         rho = qt.tensor(rho0, rho_B)
-#         H_t = make_Hamiltonian(y_uv, [b_t, b_t_g, b_t_12, b_t_g_12])
-#         U = make_propagator(H_t, t_vec)
-#         rho_MT = U @ rho.full() @ U.conjugate().transpose()
-#         output.append(qt.Qobj(rho_MT, dims = [[2,2,2],[2,2,2]]))
-#     return output
+    rho_MT = jnp.matmul(jnp.matmul(U, rho0), U.conjugate().transpose())
+    return rho_MT
 
 
-def solver_prop(y_uv, noise_mats, t_vec, **kwargs):
-    n_shots = kwargs.get('n_shots')
-    state = kwargs.get('state')
-    a_sp = kwargs.get('a_sp')
-    c = kwargs.get('c')
-    rho0 = make_init_state(a_sp, c, state = state)
-    rho_B = qt.basis(2, 0) * qt.basis(2, 0).dag()
-    rho = (qt.tensor(rho0, rho_B)).full()
-    output = Parallel(n_jobs = 24, verbose=0)(delayed(single_shot_prop)(noise_mats, t_vec, y_uv, rho) for i in range(n_shots))
+def solver_prop(y_uv, noise_mats, t_vec, rho, n_shots):
+    y_uv = jnp.array(y_uv)
+    output = []
+    # Memory allocation safety for my laptop with a single GPU
+    slice_size = 500
+    n_slices = int(np.ceil(n_shots/slice_size))
+    for i in range(n_slices):
+        n_arr = jnp.array(np.random.randint(0, 100, (slice_size, 2)))
+        result = jax.vmap(single_shot_prop, in_axes=[None, None, None, None, 0])(noise_mats, t_vec, y_uv, rho, n_arr)
+        for j in range(n_arr.shape[0]):
+            output.append(qt.Qobj(jnp.array(result[j]), dims=[[2, 2, 2], [2, 2, 2]]))
     return output
+
+
