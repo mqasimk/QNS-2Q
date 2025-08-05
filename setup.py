459d6bcb7892f5899c93c510143c28b5f1ea95df
mqasimk <mqasimk.96@gmail.com>
2025-08-05 12:51:15 -0400

    making it an installable python package.

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="QNS-2Q",
+    version="0.0.1",
+    author="Muhammad Qasim",
+    author_email="mqasimk@gmail.com",
+    description="A package for simulating quantum non-local effects in a two-qubit system.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
